3e1a262b6a71bda030640aaa25356b0fdea08fbe
Jeff.A <kinesthesiac@gmail.com>
2020-05-08 13:53:53 -0700

    Fixed an error with npm react package. Fixed some route errors.

---
+++ client/build/precache-manifest.e67e165a6cbc7583ae375ff6eb67ece2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "574d952b1ceedd36546f",
+    "url": "/static/css/main.0869c440.chunk.css"
+  },
+  {
+    "revision": "574d952b1ceedd36546f",
+    "url": "/static/js/main.22e89cd8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2f399ff611c5c4b449c9",
+    "url": "/static/css/2.ecb27a51.chunk.css"
+  },
+  {
+    "revision": "2f399ff611c5c4b449c9",
+    "url": "/static/js/2.32bfc45e.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "cb1a3b17571e0cc4e81511a2a9bda4c6",
+    "url": "/index.html"
+  }
+];
