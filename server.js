18597b886856ed1bfffcfcfb3afb48b88e8c39fb
Jeff.A <kinesthesiac@gmail.com>
2020-05-11 15:57:16 -0700

    Fixing the db and heroku.

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const mongoose = require ("mongoose");
 
 const Book = require("./models/Books");
 
-var MONGODB_URI =  process.env.MONGODB_URI || "mongodb://heroku_1b5mbwhd:4tadas89mmunku92h6ok4icueo@ds017553.mlab.com:17553/heroku_1b5mbwhd";
-
+var MONGODB_URI = "mongodb://heroku_1b5mbwhd:4tadas89mmunku92h6ok4icueo@ds017553.mlab.com:17553/heroku_1b5mbwhd";
+// process.env.MONGODB_URI
 
 // "mongodb://heroku_40lj7s5b:f9sd7msbe4vvh0dbbhk78rpprp@ds029466.mlab.com:29466/heroku_40lj7s5b"// "mongodb://localhost:27017/googlebooks"; //process.env.MONGODB_URI ||
 
@@ -19,6 +19,7 @@ var MONGODB_URI =  process.env.MONGODB_URI || "mongodb://heroku_1b5mbwhd:4tadas8
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }); 
 
 // Define API routes here
 app.get("/api/books", function(request, response) {
@@ -30,15 +31,13 @@ app.get("/api/books", function(request, response) {
 })
 
 app.post('/api/books', function(request, response) {
-
-  response.send("post books")
-  // Book.create(request.body)
-  // .then(books => {
-  //   response.json(books);
-  // })
-  // .catch(err=> {
-  //   response.status(404).json(err);
-  // });
+  Book.create(request.body)
+  .then(books => {
+    response.json(books);
+  })
+  .catch(err=> {
+    response.status(404).json(err);
+  });
 });
 
 app.delete("/api/books/:id", function(request, response) {
@@ -57,7 +56,7 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (request, response) => {
   response.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }); 
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
